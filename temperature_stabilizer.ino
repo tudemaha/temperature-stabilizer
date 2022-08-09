a1923c1a5b85848e1c2aa0d6ffcaf529ebf38094
tudemaha <gede_mahardika88@yahoo.co.id>
2022-08-09 15:58:17 +0800

    first commit

---
+++ temperature_stabilizer.ino
@@ -0,0 +1,108 @@
+#define BLYNK_TEMPLATE_ID "TMPLYcy-HC5Y"
+#define BLYNK_DEVICE_NAME "Temperature Stabilizer"
+#define BLYNK_AUTH_TOKEN "V7MSkFU9IoT6oztNMxYdkQayQ2Le5KPO"
+
+#define DHTPIN 0
+#define DHTTYPE DHT11
+
+#define BLYNK_GREEN "#23C48E"
+#define BLYNK_RED   "#D3435C"
+
+#define relay 2
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <WiFiManager.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+
+WidgetLED led1(V1);
+bool ledStatus = false;
+
+float setTemperature = 50.0f;
+BLYNK_WRITE(V7) {
+  setTemperature = param.asInt();
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  pinMode(relay, OUTPUT);
+  
+  digitalWrite(relay, LOW);
+
+  if (wifiManager()) {
+    Serial.println("WiFi Connected");
+  } else {
+    Serial.println("Failed to connect.");
+    ESP.restart();
+  }
+  
+  blynkBegin();
+    
+  dht.begin();
+  led1.on();
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop() {
+  Blynk.run();
+  timer.run();
+}
+
+bool wifiManager() {
+  WiFi.mode(WIFI_STA);
+
+  WiFiManager manager;
+
+  // enable for testing necessary
+  //  manager.resetSettings();
+
+  bool res;
+  res = manager.autoConnect("Temperature Stabilizer", "temp12345");
+
+  return res;
+}
+
+void sendSensor() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if(isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT!");
+    return;
+  }
+
+  Blynk.virtualWrite(V5, t);
+  Blynk.virtualWrite(V6, h);
+
+  if (led1.getValue()) {
+    led1.off();
+  } else {
+    led1.on();
+  }
+  
+  if(t >= setTemperature) {
+    led1.setColor(BLYNK_RED);
+    digitalWrite(relay, HIGH);
+  } else {
+    led1.setColor(BLYNK_GREEN);
+    digitalWrite(relay, LOW);
+  }
+}
+
+void blynkBegin() {
+  String __ssid = WiFi.SSID();
+  String __pass = WiFi.psk();
+
+  char ssid[__ssid.length() + 1];
+  char pass[__pass.length() + 1];
+
+  strcpy(ssid, __ssid.c_str());
+  strcpy(pass, __pass.c_str());
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+}
